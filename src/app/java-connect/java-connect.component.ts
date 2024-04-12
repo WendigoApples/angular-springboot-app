640b00e8a363b3a877d69699d0e049c90511b9f2
“cooks.johns” <cooks.johns@gmail.com>
2024-04-12 13:59:21 -0500

    fix ups to connect with spring application

---
+++ src/app/java-connect/java-connect.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-java-connect',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      java-connect works!
+    </p>
+  `,
+  styleUrl: './java-connect.component.css'
+})
+export class JavaConnectComponent {
+
+}
